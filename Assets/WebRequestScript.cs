44ea85795a8ac16a5c74b83c41a8fbd3099346ab
Nathan Loewen <loewen.nathan@gmail.com>
2016-06-13 12:24:18 -0500

    Implement UnityWebRequest bug demonstration

---
+++ Assets/WebRequestScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class WebRequestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		string testurl = "https://example.com/test%2ffile";
+		UnityWebRequest request = new UnityWebRequest();
+		request.url = testurl;
+		
+		if (request.url == testurl)
+			Debug.Log("request.url == testurl");
+		else
+		{
+			Debug.LogError("request.url != testurl\nExpected \""+testurl+"\" but got \""+request.url+"\"");
+		}
+	}
+}
